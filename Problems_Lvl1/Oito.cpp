e1cff0a532ee9ef810032b52843ac3c15071ca8a
Kayann Soares <kayan.soares1@gmail.com>
2021-01-03 15:09:03 -0300

    Update

---
+++ Problems_Lvl1/Oito.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  for(int i=0;i<=15;i++){
+
+    cout << "Dec: " << dec << i << endl;
+    cout << "Oct: " << oct << i << endl;
+    cout << "Hex: " << hex << i << endl;
+    cout << endl;
+  }
+  
+}
