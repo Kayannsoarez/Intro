f52b4fc4408f8137f72a032c130c6bc0f906920f
Kayann Soares <kayan.soares1@gmail.com>
2021-01-06 00:10:08 -0300

    Update

---
+++ Problems_Lvl1/quatorze.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  
+  string nome, sobrenome;
+
+  cout << "Digite o Nome: " << endl;
+  cin >> nome;
+
+  cout << "Digite o Sobrenome: " << endl;
+  cin >> sobrenome;
+
+  cout << nome +" "+ sobrenome;
+
+  return 0;
+
+}
