035013b440fefa2a180a464cce290edaf8add653
Kayann Soares <kayan.soares1@gmail.com>
2021-01-19 23:33:49 -0300

    update

---
+++ Problems_Lvl1/Dezoito.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    float FV, pv,i,n;
+
+    cout << "Digite Montante:" << endl;
+    cin >> pv;
+    cout << "Digite Taxa de Rendimento:" << endl;
+    cin >> i;
+    cout << "Digite Quantidade de Periodos:" << endl;
+    cin >> n;
+
+    FV = (pv*pow((1+(i/100)), n));
+    cout << "Valor Total: " << FV << endl;
+
+    return 0;
+}
