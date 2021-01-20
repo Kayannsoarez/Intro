9b2f71c25795900768046c5150bd73880c9d1910
Kayann Soares <kayan.soares1@gmail.com>
2021-01-19 23:36:07 -0300

    Update

--- Problems_Lvl1/Dezoito.cpp
+++ Problems_Lvl1/Dezoito.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 int main(){
 
-    float FV, pv,i,n;
+  float FV, pv,i,n;
 
-    cout << "Digite Montante:" << endl;
-    cin >> pv;
-    cout << "Digite Taxa de Rendimento:" << endl;
-    cin >> i;
-    cout << "Digite Quantidade de Periodos:" << endl;
-    cin >> n;
+  cout << "Digite Montante:" << endl;
+  cin >> pv;
+  cout << "Digite Taxa de Rendimento:" << endl;
+  cin >> i;
+  cout << "Digite Quantidade de Periodos:" << endl;
+  cin >> n;
 
-    FV = (pv*pow((1+(i/100)), n));
-    cout << "Valor Total: " << FV << endl;
+  FV = (pv*pow((1+(i/100)), n));
+  cout << "Valor Total: " << FV << endl;
 
-    return 0;
+  return 0;
 }
